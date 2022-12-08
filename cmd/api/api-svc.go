3daa10ed2dce8f98c6aadee0decf95d92aa40e2b
stanislavdimitrenco <stanislav.dimitrenko@gmail.com>
2022-12-08 23:34:52 +0100

    created server

--- cmd/api/api-svc.go
+++ cmd/api/api-svc.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("api/1", health)
+	mux.HandleFunc("api/1/protected", health)
+
+	server := http.Server{
+		Addr:         ":8888",
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 10,
+	}
+
+	error := server.ListenAndServe()
+
+	log.Printf("Starting server on: %s", server.Addr)
+
+	if error != nil {
+		log.Fatalf("Error %v/n", error.Error())
+	}
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
 
 }
