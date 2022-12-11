64f6269343390fb058e935a9e0acc1625a68b845
stanislavdimitrenco <stanislav.dimitrenko@gmail.com>
2022-12-11 22:26:51 +0100

    added base auth

--- cmd/api/api-svc.go
+++ cmd/api/api-svc.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 )
 
+var authData = auth{
+	User:     "stas",
+	Password: "1111",
+}
+
+type auth struct {
+	User     string
+	Password string
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/api/1", health)
-	mux.HandleFunc("/api/1/protected", health)
+	mux.HandleFunc("/api/v1", basicAuth(health))
+	mux.HandleFunc("/api/v1/protected", basicAuth(protected))
 
 	server := http.Server{
 		Addr:         ":8888",
@@ -25,12 +36,47 @@ func main() {
 
 	error := server.ListenAndServe()
 
-
 	if error != nil {
 		fmt.Errorf("Error %v/n", error.Error())
 	}
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
+	json, _ := json.Marshal(map[string]interface{}{"status": "ok", "service": "api"})
+	w.Write(json)
+
+}
+
+func protected(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	(w).Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+	json, _ := json.Marshal(map[string]interface{}{"status": "ok", "service": "api"})
+	w.Write(json)
+
+}
+
+func basicAuth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, password, ok := r.BasicAuth()
+
+		if !ok {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		if user != authData.User || password != authData.Password {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		handler.ServeHTTP(w, r)
+
+	}
 }
